1255951f83b8e9950ec309f4c537128776ffe140
Maree Fourie <MareeF@k2.com>
2020-02-10 13:51:58 +0200

    Code contracts

--- Code/Template/Template/Class1.cs
+++ Code/Template/Template/Class1.cs
@@ -1,19 +1,43 @@
-﻿using System;
+﻿using static CodeContract.Contract;
 
 namespace Template
 {
-    public class CProgram : IProgram
-    {
-
-    }
-
-    public interface IProgram
-    {
-
-    }
-
     public class Contract
     {
+    }
 
+    public class Account
+    {
+        private int _limit;
+        public int Limit
+        {
+            get => _limit;
+            set
+            {
+                Require(value > 0, "Limit must not be 0.");
+                _limit = value;
+                ClassInvariant();
+            }
+        }
+        public int Total { get; private set; }
+        public int Add(int value, int discount)
+        {
+            Require(value >= 0, "Value must be positive.")
+                .Require(
+                    discount >= 0 && discount <= 50, 
+                    "Discount must be positive and not more than 50%");
+            var old = new { Total };
+
+            // method logic
+
+            Ensure(Total > old.Total, "The total will not decrease.");
+            ClassInvariant();
+            return discountedValue;
+        }
+        private void ClassInvariant()
+        {
+            Invariant(Total >= 0, "The total will not be negative.")
+                .Invariant(Limit > 0, "Limit will not be 0.");
+        }
     }
 }
---
+++ Code/Template/Template/Contract.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+
+namespace CodeContract
+{
+
+    public static class Contract
+    {
+        public static Requirer Require(bool condition, string message)
+        {
+            if (!condition)
+                throw new ConstactRequireException(message);
+            else
+                return new Requirer();
+        }
+
+        public static Ensurer Ensure(bool condition, string message)
+        {
+            if (!condition)
+                throw new ConstactEnsureException(message);
+            else
+                return new Ensurer();
+        }
+
+        public static Invarianter Invariant(bool condition, string message)
+        {
+            if (!condition)
+                throw new ConstactInvariantException(message);
+            else
+                return new Invarianter();
+        }
+    }
+
+    public class Invarianter
+    {
+        public Invarianter Invariant(bool condition, string message)
+        {
+            if (!condition)
+                throw new ConstactEnsureException(message);
+            else
+                return this;
+        }
+    }
+
+    public class Ensurer
+    {
+        public Ensurer Ensure(bool condition, string message)
+        {
+            if (!condition)
+                throw new ConstactEnsureException(message);
+            else
+                return this;
+        }
+    }
+
+    public class Requirer
+    {
+        public Requirer Require(bool condition, string message)
+        {
+            if (!condition)
+                throw new ConstactRequireException(message);
+            else
+                return this;
+        }
+    }
+
+    public class ConstactRequireException : Exception
+    {
+        public ConstactRequireException(string message)
+            : base(message)
+        { }
+    }
+
+    public class ConstactEnsureException : Exception
+    {
+        public ConstactEnsureException(string message)
+            : base(message)
+        { }
+    }
+
+    public class ConstactInvariantException : Exception
+    {
+        public ConstactInvariantException(string message)
+            : base(message)
+        { }
+    }
+}
